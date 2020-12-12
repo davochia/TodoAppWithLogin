09fdb28d7f44d956a32bef80a0dbb7e1140fef16
King David <47652874+davochia@users.noreply.github.com>
2020-12-12 19:33:42 +0200

    Create main.tf
    added terraform

---
+++ main.tf
@@ -0,0 +1,71 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "devops" {
+  name     = "devops_2020"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "devops" {
+  name                = "network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.devops.location
+  resource_group_name = azurerm_resource_group.devops.name
+}
+
+resource "azurerm_subnet" "devops" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.devops.name
+  virtual_network_name = azurerm_virtual_network.devops.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "devops" {
+  name                = "network-interface"
+  location            = azurerm_resource_group.devops.location
+  resource_group_name = azurerm_resource_group.devops.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.devops.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "devops" {
+  name                = "virtual-machine"
+  resource_group_name = azurerm_resource_group.devops.name
+  location            = azurerm_resource_group.devops.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.devops.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
