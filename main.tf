00b5b7ca500afc633a75da35a5d7dc922d214406
King David <47652874+davochia@users.noreply.github.com>
2020-12-13 00:46:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,10 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.26"
-    }
-  }
-}
-
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {}
+    # The "feature" block is required for AzureRM provider 2.x. 
+    # If you're using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
 }
-
 
 # Create a resource group if it doesn't exist
 resource "azurerm_resource_group" "myterraformgroup" {
